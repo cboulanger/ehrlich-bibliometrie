d84c77a96b70db1c8ba1ae2fc6735f8b02350c78
Christian Boulanger <info@bibliograph.org>
2023-09-13 14:09:29 +0200

    Updates

--- scripts/pyvis.py
+++ scripts/pyvis.py
@@ -4,18 +4,35 @@ from IPython.display import display, HTML, Image
 from typing import Union
 from textwrap import shorten
 import subprocess
+import json
 
+def strip_property_prefix(input_dict, prefix):
+    output_dict = {}
+    for key, value in input_dict.items():
+        if key.startswith(prefix):
+            new_key = key[len(prefix):]
+        else:
+            new_key = key
+        output_dict[new_key] = value
+    return output_dict
 
-def py2neo_to_pyvis(net: Network, obj: Union[Path, Node, Relationship], auto_rel_label=False, edge_default_width=3):
+def py2neo_to_pyvis(net: Network,
+                    obj: Union[Path, Node, Relationship],
+                    auto_rel_label=False,
+                    edge_default_width=3,
+                    font_default_size=20):
     if type(obj) is Path:
         for o in walk(obj):
             py2neo_to_pyvis(net, o)
     elif type(obj) is Node:
-        label = obj['display_name'] or obj['title'] or obj['name'] or obj['id'] or ''
-        obj['label'] = shorten(label, width=50, placeholder="...").replace(':', ':\n')
-        obj['group'] = obj['group'] or str(obj.labels)
-        obj['font'] = obj['font'] or {'size': 20}
-        net.add_node(obj.identity, **obj)
+        p = strip_property_prefix(dict(obj), "vis_")
+        label = p['display_name'] or p['title'] or p['name'] or p['id'] or ''
+        p['label'] = shorten(label, width=50, placeholder="...").replace(':', ':\n')
+        if 'group' not in p:
+            p['group'] = str(obj.labels)
+        if 'font' not in p:
+            p['font'] = {'size': font_default_size}
+        net.add_node(obj.identity, **p)
     elif issubclass(type(obj), Relationship):
         start_node = obj.start_node
         end_node = obj.end_node
@@ -26,28 +43,34 @@ def py2neo_to_pyvis(net: Network, obj: Union[Path, Node, Relationship], auto_rel
         py2neo_to_pyvis(net, start_node)
         py2neo_to_pyvis(net, end_node)
         neo4j_label = type(obj).__name__
-        if obj['title'] is None:
-            obj['title'] = neo4j_label
-        if obj['label'] is None:
-            obj['label'] = (neo4j_label if auto_rel_label else None)
-        if obj['group'] is None:
-            obj['group'] = neo4j_label
-        if obj['width'] is None:
-            obj['width'] = edge_default_width
-        net.add_edge(start_node.identity, end_node.identity, **obj)
+        p = strip_property_prefix(dict(obj), "vis_")
+        if 'title' not in p:
+            p['title'] = neo4j_label
+        if 'label' not in p:
+            p['label'] = (neo4j_label if auto_rel_label else None)
+        if 'group' not in p:
+            p['group'] = neo4j_label
+        if 'width' not in p:
+            p['width'] = edge_default_width
+        net.add_edge(start_node.identity, end_node.identity, **p)
 
 
 def create_or_update_network(graph: Graph,
                              query: str,
-                             height: str = "600px",
-                             width: str = "100%",
+                             height: str = "300px",
                              auto_rel_label=False,
                              net: Network = None,
+                             seed: int = None,
                              **kwargs) -> Network:
     data = graph.run(query, **kwargs).data()
     if net is None:
-        net = Network(height=height, width=width, notebook=True, cdn_resources='in_line', directed=True)
-        net.force_atlas_2based(overlap=0.7)
+        net = Network(height, notebook=True, cdn_resources='in_line', directed=True)
+        net.force_atlas_2based(overlap=0.7, damping=1)
+        if seed is not None:
+            options = json.loads(net.options.to_json())
+            options['layout'] = {"randomSeed": seed, "improvedLayout": True}
+            options = json.dumps(options)
+            net.set_options(options)
     for row in data:
         for obj in row.values():
             py2neo_to_pyvis(net, obj, auto_rel_label=auto_rel_label)
@@ -91,3 +114,16 @@ def draw(graph: Graph,
          **kwargs):
     net = create_or_update_network(graph, query, height=height, auto_rel_label=auto_rel_label, **kwargs)
     return draw_network(net, file=file, link_only=not do_display)
+
+def cleanup(graph: Graph):
+    # remove styling
+    graph.run("""
+    MATCH (n) WHERE any(key IN keys(n) WHERE key STARTS WITH 'vis_')
+    WITH n, [key IN keys(n) WHERE key STARTS WITH 'vis_'] AS keys
+    CALL apoc.create.removeProperties(n, keys) YIELD node RETURN node;
+    """)
+    graph.run("""
+    MATCH ()-[r]-() WHERE any(key IN keys(r) WHERE key STARTS WITH 'vis_')
+    WITH r, [key IN keys(r) WHERE key STARTS WITH 'vis_'] AS keys
+    CALL apoc.create.removeRelProperties(r, keys) YIELD rel RETURN rel;
+    """)
